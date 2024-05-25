9d63acb11e73e07f44a130a5a248667c65693950
Guillaume Sorel <guillaumesrl@gmail.com>
2024-05-25 16:01:45 +0200

    add of rainbow functon + clear function

--- main.js
+++ main.js
@@ -1,29 +1,66 @@
 const grid = document.querySelector('.grid')
+let size = 16;
+let colorBool = false;
 
-function clearGrid() {
+function clearDiv() {
     grid.innerHTML = '';
 }
 
+function getColor() {
+    const r = Math.floor(Math.random(1) * 256);
+    const g = Math.floor(Math.random(1) * 256);
+    const b = Math.floor(Math.random(1) * 256);
+    console.log(`rgb(${r},${g},${b})`);
+    return `rgb(${r},${g},${b})`;
+}
+
 function generateGrid(size) {
-    clearGrid();
+    clearDiv();
     for (let i = 0; i < size * size; i++) {
+        let color = "blue";
         const item = document.createElement('div');
         item.classList.add('item');
         item.style.flex = `0 0 calc(100% / ${size})`;
         grid.appendChild(item);
-        item.addEventListener("mouseover", () => item.style.backgroundColor = "blue");
-        }
+        item.addEventListener("mouseover", () => {
+            let color = (colorBool) ? getColor() : "blue";
+            
+            item.style.backgroundColor = color
+        });
+    }
 };
 
-generateGrid(50);
+generateGrid(size);
 
 const sizeBtn = document.querySelector("#size")
 sizeBtn.addEventListener("click", () => {
-    let size = 0;
+    size = 0;
     do {
         size = parseInt(prompt("Enter the size of the grid from 1 to 100 :"));
-    } while (size === 0 || size > 100);
+    } while (size < 1 || size > 100);
     generateGrid(size);
-})
+});
+
+function clearGrid() {
+    generateGrid(size);
+}
+
+const clearBtn = document.querySelector("#clear");
+clearBtn.addEventListener("click", () => {
+    clearGrid();
+});
+
+const rainbowButton = document.querySelector("#rainbow")
+rainbowButton.addEventListener("click", () => {
+    if (rainbowButton.textContent === "Activate rainbow") {
+        colorBool = true;
+        rainbowButton.textContent = "Back to blue";
+    } else {
+        colorBool = false;
+        rainbowButton.textContent = "Activate rainbow"
+    }
+});
+
+
 
 
